f2ca15d21105c8abdd12cb0a19f03fa404e858ee
Haishi2016 <hbai@microsoft.com>
2017-09-30 20:52:38 -0700

    Chapter 1 - Hello World application

---
+++ Chapter-1/HelloWorldApplication/HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace HelloWorldService
+{
+    internal sealed class HelloWorldService : StatelessService
+    {
+        public HelloWorldService(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[0];
+        }
+
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                ServiceEventSource.Current.ServiceMessage(this.Context, "Hello World at " + DateTime.Now.ToLongTimeString());
+                await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ProgrammingServiceFabric
+# Programming Service Fabric
 Source code for the Programming Microsoft Azure Service Fabric book
+
+## Version 2 Source Code
+
+### Chapter 1
+
+* [HelloWorldApplication](./Chapter-1/HelloWorldApplication)
+
+    The Hello, World! application of Service Fabric.
+
+## Version 1 Source Code
+
+The following folders contain projects from the version 1 book that are not brought forward to version 2.
