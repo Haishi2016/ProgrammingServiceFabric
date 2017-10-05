b092842e7e08ca0fa7d0f2776da3a300b6757e36
Haishi Bai <hbai@microsoft.com>
2017-10-05 12:49:46 -0700

    A more appropriate remoting sample

---
+++ Chapter-2/CalculatorApplication/V1/CalculatorWeb/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using CalculatorService.Interfaces;
+
+namespace CalculatorWeb.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public int Add(int a, int b)
+        {
+            var calculatorClient = ServiceProxy.Create<ICalculatorService>(
+               new Uri("fabric:/CalculatorApplication/CalculatorService"));
+            return calculatorClient.Add(1, 2).Result;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public int Subtract(int a, int b)
+        {
+            var calculatorClient = ServiceProxy.Create<ICalculatorService>(
+               new Uri("fabric:/CalculatorApplication/CalculatorService"));
+            return calculatorClient.Subtract(1, 2).Result;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
