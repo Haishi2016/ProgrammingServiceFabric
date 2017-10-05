8285a91dc97603a901105762074ba53907b7aab9
Haishi2016 <hbai@microsoft.com>
2017-10-04 23:42:26 -0700

    Calculator Service V 1.5

---
+++ Chapter-2/CalculatorApplication/V1.5/CalculatorService.Interfaces/ICalculatorService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.FabricTransport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: FabricTransportServiceRemotingProvider(RemotingListener = RemotingListener.V2Listener, RemotingClient = RemotingClient.V2Client)]
+namespace CalculatorService.Interfaces
+{
+    public interface ICalculatorService: IService
+    {
+        Task<long> Add(int a, int b);
+        Task<long> Subtract(int a, int b);
+    }
+}
---
+++ Chapter-2/CalculatorApplication/V1.5/CalculatorService/CalculatorService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using CalculatorService.Interfaces;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.V2.FabricTransport.Runtime;
+
+namespace CalculatorService
+{
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class CalculatorService : StatelessService, ICalculatorService
+    {
+        public CalculatorService(StatelessServiceContext context)
+            : base(context)
+        { }
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new[]
+            {
+                new ServiceInstanceListener((c) =>
+                    {
+                        return new FabricTransportServiceRemotingListener(c, this, null,
+                        new ServiceRemotingJsonSerializationProvider());
+                    })
+            };
+
+        }
+        public Task<long> Add(int a, int b)
+        {
+            return Task.FromResult<long>(a + b);
+        }
+        public Task<long> Subtract(int a, int b)
+        {
+            return Task.FromResult<long>(a - b);
+        }
+    }
+}
