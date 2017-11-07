eadc98ad98ebcfe61c2a037c51034a0e0f1c5f7f
Haishi2016 <hbai@microsoft.com>
2017-11-07 11:21:57 -0800

    chapter 6 samples

---
+++ Chapter-5/NodeJsHelloWorldApplication/NodeJsHelloWorldApplication/ApplicationPackageRoot/NodeAppPkg/Code/installNode.cmd
@@ -0,0 +1,4 @@
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+cinst nodejs.install -y
+SET PATH=%PATH%;C:\Program Files\nodejs
+npm install
---
+++ Chapter-6/ConfigurationUpdate/Stateless1/Stateless1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace Stateless1
+{
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class Stateless1 : StatelessService
+    {
+        int incrementStep = 1;
+        public Stateless1(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[0];
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            int iterations = 0;
+            this.Context.CodePackageActivationContext.ConfigurationPackageModifiedEvent += CodePackageActivationContext_ConfigurationPackageModifiedEvent;
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", iterations += incrementStep);
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+
+        private void CodePackageActivationContext_ConfigurationPackageModifiedEvent(object sender, PackageModifiedEventArgs<ConfigurationPackage> e)
+        {
+            var configSection = this.Context.CodePackageActivationContext.GetConfigurationPackageObject("Config");
+            var text = configSection.Settings.Sections["MyConfigSection"].Parameters["IncrementStep"].Value;
+            incrementStep = int.Parse(text);
+        }
+    }
+}
